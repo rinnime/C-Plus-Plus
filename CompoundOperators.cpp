d130406326a177c3c828aa96f4f1e842cc9ebf9e
Corin <48391599+rinnime@users.noreply.github.com>
2023-10-10 18:54:38 -0400

    Add files via upload

---
+++ CompoundOperators.cpp
@@ -0,0 +1,17 @@
+// Variables&Operators.cpp 
+
+#include <iostream>
+
+int main()
+{
+	int x = 10;
+
+	x += 20; //same as x = x + 20;
+	x -= 10; //same as x = x - 10;
+	x *= 10; //same as x = x * 10;
+	x /= 10; //same as x = x / 10;
+	x %= 10; //same as x = x % 10;
+
+
+
+}
