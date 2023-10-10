e659f01ae8b72942f892bcfdf600922a589e549f
Corin <48391599+rinnime@users.noreply.github.com>
2023-10-10 18:50:19 -0400

    Add files via upload

---
+++ Variables.cpp
@@ -0,0 +1,36 @@
+// Variables.cpp
+
+#include <iostream>
+#include  <string>
+
+using namespace std;
+
+int main()
+{
+	// dataType variableName;
+	int age;
+	float weight;
+	double distance;
+	bool isAlive;
+	char initial;
+	string name; //the string lives inside the namespace of std.
+
+	unsigned short int betterAge;
+	long double hugeDistance;
+
+	age = 26;
+	weight = 170.5f; //add f to know the difference between float and double.
+	distance = 1212.987;
+	isAlive = true;
+	initial = 'C';
+	name = "Corin";
+
+	cout << "My name is " << name << " and I am " << age << " years old." << endl;
+	cout << "Am I alive: " << isAlive << endl;
+
+	int newAge;
+	newAge = age; // is also equal to 26
+
+	int newestAge = 27;
+}
+
