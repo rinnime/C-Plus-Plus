c5fadad97bf36049f6a0db5dbb0522b94cfc9445
Corin <48391599+rinnime@users.noreply.github.com>
2023-10-10 18:50:35 -0400

    Add files via upload

---
+++ Integers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 10;
+	int b = 3;
+
+	int add = a + b;
+	int subtract = 10 - b;
+	int multiply = a * 3;
+	int division = a / b;
+	int mod = a % b;
+
+	int result = 1 + 2 * 3 + 4;
+
+	float div2 = 10 / 3.0f;
+
+	cout << add << endl;
+	cout << subtract << endl;
+	cout << multiply << endl;
+	cout << div2 << endl;
+	cout << mod << endl;
+	cout << result << endl;
+	
+}
