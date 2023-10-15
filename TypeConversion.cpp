7d5ba5e308746fd03dad6a60878b97764e8ae418
Corin <48391599+rinnime@users.noreply.github.com>
2023-10-15 07:14:28 -0400

    Add files via upload

---
+++ TypeConversion.cpp
@@ -0,0 +1,20 @@
+// TypeConversion.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    float myFloat = 30.5f;
+    int myInt = myFloat;
+
+    int a = 20;
+    float b = (float)a;
+
+    int result = (int) (2.4 + 1.6);
+
+    cout << result << endl;
+
+}
